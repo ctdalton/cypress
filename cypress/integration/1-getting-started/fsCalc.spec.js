e85fa4976818c0237cdfee30919390cbc218123f
ctdalton <curtis.dalton1@gmail.com>
2021-10-12 16:58:03 -0600

    created fsCalc spec

---
+++ cypress/integration/1-getting-started/fsCalc.spec.js
@@ -0,0 +1,18 @@
+//Intelligent Code Completion
+/// <reference types="Cypress" />
+
+describe('My First Test', () => {
+    it('Gets, types and asserts', () => {
+      cy.visit('https://fs-calc.herokuapp.com/')
+  
+      cy.get('#startNewCalculation').click()
+  
+      // Should be on a new URL which includes '/basicCalc'
+      cy.url().should('include', '/basicCalc')
+  
+      // Get an input, type into it and verify that the value has been updated
+      cy.get('#adults')
+        .type('7')
+        .should('have.value', '7')
+    })
+  })
--- cypress/integration/1-getting-started/sample_spec.js
+++ cypress/integration/1-getting-started/sample_spec.js
@@ -1,3 +1,6 @@
+//Intelligent Code Completion
+/// <reference types="Cypress" />
+
 describe('My First Test', () => {
     it('Gets, types and asserts', () => {
       cy.visit('https://example.cypress.io')
